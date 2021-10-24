25cd85fd59b9704c6285a2dca589e6b1ce39ad62
Maximo Guk <62088388+Maximo-Guk@users.noreply.github.com>
2021-10-24 20:35:46 -0300

    Content-Type access header

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ const router = Router();
 function cors(response: Response) {
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Methods', 'POST, GET, DELETE');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
   response.headers.set('Access-Control-Max-Age', '86400');
   return response;
 }
