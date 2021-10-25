14f18982e792e116f3e0404a6f2bc894d0b49568
Maximo Guk <62088388+Maximo-Guk@users.noreply.github.com>
2021-10-25 18:32:21 -0300

    updated with new tunnel endpoints

--- src/components/authJwt.ts
+++ src/components/authJwt.ts
@@ -1,7 +1,7 @@
 // Fetch GET Implementation
 export default async function verifyJwt(userName: string): Promise<unknown> {
   // API Endpoint
-  const url = `https://march-sc-choose-email.trycloudflare.com/auth/${userName}`;
+  const url = `https://lamp-itself-nsw-vocals.trycloudflare.com/auth/${userName}`;
   const response = await fetch(url, {
     method: 'GET',
     headers: {
--- src/components/verifyJwt.ts
+++ src/components/verifyJwt.ts
@@ -1,7 +1,7 @@
 // Fetch GET Implementation
 export default async function verifyJwt(idToken: string): Promise<unknown> {
   // API Endpoint
-  const url = 'https://march-sc-choose-email.trycloudflare.com/verify';
+  const url = 'https://lamp-itself-nsw-vocals.trycloudflare.com/verify';
   const response = await fetch(url, {
     method: 'GET',
     headers: {
