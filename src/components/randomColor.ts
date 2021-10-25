4c0fe4402e36a0b046930b15646bd934f34724f3
Maximo Guk <62088388+Maximo-Guk@users.noreply.github.com>
2021-10-24 22:00:41 -0300

    Updated colors with hex

--- src/components/randomColor.ts
+++ src/components/randomColor.ts
@@ -1,15 +1,15 @@
 const colors = [
-  'blue',
-  'brown',
-  'cyan',
-  'green',
-  'indigo',
-  'orange',
-  'pink',
-  'purple',
-  'red',
-  'teal',
-  'yellow',
+  '#2196f3', //blue 500
+  '#795548', //brown 500
+  '#00bcd4', //cyan 500
+  '#4caf50', //green 500
+  '#3f51b5', //indigo 500
+  '#ff9800', //orange 500
+  '#e91e63', //pink 500
+  '#9c27b0', //purple 500
+  '#f44336', //red 500
+  '#009688', //teal 500
+  '#ffeb3b', //yellow 500
 ];
 
 export default function randomColor(): string {
